7a9eae49dc451836b3198bd3baf5793ca67db9d5
anthonytissue <anthony.tissue@patelhs.org>
2021-02-26 13:13:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Welcome to Anthonys wing shop, how many sauces would you like with your wings?"); 
+    int wings = scan.nextInt();//asks for user imput
+    int number = 1;// depending on the number of sauces you pick, the recursion will let you choose that many sauces
+    chickenWings(wings, number); //calling method right here
+  }
+    static boolean chickenWings(int wings, int number) {
+      Scanner scan = new Scanner(System.in);
+      if (number <= wings) {
+        System.out.println("You have ordered " + number + " sauces. ");
+        System.out.println("What sauce would you like with your wings?");;
+        String sauce = scan.next();
+        number ++;
+        return chickenWings(wings, number);
+      } else {
+        System.out.println("Come and get your wings!");
+
+        return false;
+      }
+    }
+}
